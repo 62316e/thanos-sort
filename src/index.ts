f3582698106d882b3e396bf6aa6224cf5b6166df
62316e <62316e@gmail.com>
2026-04-02 22:51:59 +0300

    Snaps 3x faster

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 export const thanosSort = (arr: number[]): number[] => {
   let n = arr.length;
 
-  while (true) {
-    let i: number = 0;
+  while (n > 1) {
+    let sorted = true;
 
-    while (i < n - 1 && arr[i] <= arr[i + 1]) {
-      i++;
+    for (let i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        sorted = false;
+        break;
+      }
     }
 
-    if (i === n - 1) return arr.slice(0, n);
-
-    n = Math.floor(n / 2);
-    arr = arr.slice(0, n);
+    if (sorted) break;
+    n >>= 1;
   }
+
+  return arr.slice(0, n);
 };
